98cb6766bf5d22aa77e70e44e8da9dd5461ca1db
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-02-21 02:31:50 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
+// vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, './src/menu.html'),
+        setdinner: resolve(__dirname, './src/setdinner.html'),
+      },
+    },
+  },
+})
