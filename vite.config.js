7a2d9989ca66258345f9e5e53df24781ad0eb65a
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-03-14 02:42:07 +0000

    Moved index html to src

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,33 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
 export default defineConfig({
+  root,
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, './menu'),
-        setdinner: resolve(__dirname, './setdinner'),
-      },
-    },
-  },
+        main: resolve(root, 'index.html'),
+        menu: resolve(root, '.menu.html'),
+        setdinner: resolve(root, '.setdinner.html')
+      }
+    }
+  }
 })
+
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         menu: resolve(__dirname, './menu'),
+//         setdinner: resolve(__dirname, './setdinner'),
+//       },
+//     },
+//   },
+// })
